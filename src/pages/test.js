3d6d960e03342f88b1811bef9bbc2af9149ba58a
mnuxD <mbaellav@uni.pe>
2022-05-18 19:22:50 -0500

    usando faas

--- src/pages/test.js
+++ src/pages/test.js
@@ -1,5 +1,17 @@
-import * as React from "react"
-
+import React, { useState, useEffect } from "react"
+import axios from "axios"
 export default function Component() {
-  return "Hello world"
+  const [message, setMessage] = useState("")
+
+  const fetchData = async () => {
+    const data = await axios.get("../api/hello-world")
+    console.log("DATA", data)
+    setMessage(data.data.message)
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
+  return <h1>El texto del services es: {message}</h1>
 }
